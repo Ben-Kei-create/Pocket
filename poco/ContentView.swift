d3ed7256ada5a291ed15d2c5a0d3ed047c57b254
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-07-26 21:55:14 +0900

    Initial Commit

---
+++ poco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  poco
+//
+//  Created by 茂木史明 on 2026/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ poco/pocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocoApp.swift
+//  poco
+//
+//  Created by 茂木史明 on 2026/07/26.
+//
+
+import SwiftUI
+
+@main
+struct pocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
